565afc5da2ebf092e58d02616ef72c7db0cc565e
Gabrielly Gonçalves <83105480+Gabrielly77@users.noreply.github.com>
2021-10-20 16:42:10 -0300

    Update .c

--- .c
+++ .c
@@ -5,7 +5,7 @@ int main(void){
 printf("Digite um número que deseja saber o resultado da tabuada: ");
 scanf("%i", &tabuada);
 
-for(int i-i; i <=10; ++i){
+for(int i - i; i <=10; ++i){
   prinft("\n%i X %i = %i, tabuada, i, i " tabuada);
 }
 return 0;
