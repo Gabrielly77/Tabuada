c4043b22e51f8ff7e5ab8ed86a6f1f2408b54106
Gabrielly Gonçalves <83105480+Gabrielly77@users.noreply.github.com>
2021-10-20 16:40:05 -0300

    Create .c

---
+++ .c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+int main(void){
+  int tabuada;
+
+printf("Digite um némero que deseja saber o resultado da tabuada!");
+scanf("%i", &tabuada);
+
+for(int i-i; i <=10; ++i){
+  prinft("\n%i X %i = %i, tabuada, i, i " tabuada);
+}
+return 0;
+
+}
